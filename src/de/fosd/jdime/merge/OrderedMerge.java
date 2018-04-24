e62657b85deb77d13a61c4be550c049f1e1361c6
paulzfm <zhufengminpaul@163.com>
2018-04-24 16:49:52 +0800

    update ordered merge

--- src/de/fosd/jdime/merge/OrderedMerge.java
+++ src/de/fosd/jdime/merge/OrderedMerge.java
@@ -4,8 +4,10 @@ import de.fosd.jdime.artifact.Artifact;
 import de.fosd.jdime.config.merge.MergeContext;
 import de.fosd.jdime.config.merge.MergeScenario;
 import de.fosd.jdime.config.merge.Revision;
+import de.fosd.jdime.operations.ConflictOperation;
 import de.fosd.jdime.operations.MergeOperation;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
@@ -55,6 +57,8 @@ public class OrderedMerge<T extends Artifact<T>> extends BasicMerge<T> implement
         T leftChild = leftIt.next();
         T rightChild = rightIt.next();
 
+        List<T> suspended = new ArrayList<>();
+
         /**
          * Ordered merge for each element.
          * Cases:
@@ -66,7 +70,7 @@ public class OrderedMerge<T extends Artifact<T>> extends BasicMerge<T> implement
          **/
         while (leftChild != null || rightChild != null) {
             if (leftChild != null && leftChild.isMerged()) { // 1) left is merged
-                LOG.fine(String.format("Ordered: %s", show(leftChild)));
+                LOG.fine(String.format("Ordered: skip merged: %s", show(leftChild)));
 
                 if (leftIt.hasNext()) {
                     leftChild = leftIt.next();
@@ -76,7 +80,7 @@ public class OrderedMerge<T extends Artifact<T>> extends BasicMerge<T> implement
                 continue;
             }
             if (rightChild != null && rightChild.isMerged()) { // 1) right is merged
-                LOG.fine(String.format("Ordered: %s", show(rightChild)));
+                LOG.fine(String.format("Ordered: skip merged: %s", show(rightChild)));
 
                 if (rightIt.hasNext()) {
                     rightChild = rightIt.next();
@@ -88,8 +92,8 @@ public class OrderedMerge<T extends Artifact<T>> extends BasicMerge<T> implement
 
             if (leftChild != null &&
                     !leftChild.getProperMatch(r, context).isPresent()) { // 2) left has no proper match
-                LOG.fine(String.format("Ordered: %s", show(leftChild)));
-                simpleMerge(leftChild, target, context, l, r, b, true);
+                LOG.fine(String.format("Ordered: suspend: %s", show(leftChild)));
+                suspended.add(leftChild);
 
                 if (leftIt.hasNext()) {
                     leftChild = leftIt.next();
@@ -100,8 +104,8 @@ public class OrderedMerge<T extends Artifact<T>> extends BasicMerge<T> implement
             }
             if (rightChild != null &&
                     !rightChild.getProperMatch(l, context).isPresent()) { // 2) right has no proper match
-                LOG.fine(String.format("Ordered: %s", show(rightChild)));
-                simpleMerge(rightChild, target, context, r, l, b, false);
+                LOG.fine(String.format("Ordered: suspend: %s", show(rightChild)));
+                suspended.add(rightChild);
 
                 if (rightIt.hasNext()) {
                     rightChild = rightIt.next();
@@ -113,6 +117,10 @@ public class OrderedMerge<T extends Artifact<T>> extends BasicMerge<T> implement
 
             if (leftChild != null && rightChild != null &&
                     leftChild.getProperMatch(r, context).get() == rightChild) {
+                // first handle suspending ones
+                handleSuspended(suspended, b, l, r, target, context);
+                suspended.clear();
+
                 // 3) (left, right) forms a proper match
                 LOG.fine(String.format("Ordered: %s, %s", show(leftChild), show(rightChild)));
                 twoOrThreeWayMerge(leftChild, rightChild, target, b, context);
@@ -130,7 +138,70 @@ public class OrderedMerge<T extends Artifact<T>> extends BasicMerge<T> implement
                 continue;
             }
 
-            assert false;
+            LOG.severe("Ordered: assertion failure: skip not matched: " +
+                    show(leftChild) + " <-> " + show(rightChild));
+            if (leftIt.hasNext()) {
+                leftChild = leftIt.next();
+            } else {
+                leftChild = null;
+            }
+            if (rightIt.hasNext()) {
+                rightChild = rightIt.next();
+            } else {
+                rightChild = null;
+            }
         }
+
+        handleSuspended(suspended, b, l, r, target, context);
+        suspended.clear();
+    }
+
+    private void handleSuspended(List<T> suspended, Revision b, Revision l, Revision r,
+                                 T target, MergeContext context) {
+        int leftCount = 0;
+        int rightCount = 0;
+
+        for (T n : suspended) {
+            if (n.getRevision().equals(l)) {
+                leftCount++;
+            } else if (n.getRevision().equals(r)) {
+                rightCount++;
+            } else {
+                LOG.severe("Ordered: handle suspend: WTF revision: " + n.getRevision());
+            }
+        }
+
+        if (rightCount == 0) { // all left
+            for (T n : suspended) {
+                simpleMerge(n, target, context, l, r, b, true);
+            }
+            return;
+        }
+        if (leftCount == 0) { // all right
+            for (T n : suspended) {
+                simpleMerge(n, target, context, r, l, b, false);
+            }
+            return;
+        }
+
+        // conflict
+        LOG.fine("Ordered: handle suspend: conflict");
+
+        T lefts = target.copy();
+        lefts.clearChildren();
+        T rights = target.copy();
+        rights.clearChildren();
+
+        for (T n : suspended) {
+            if (n.getRevision().equals(l)) {
+                lefts.addChild(n);
+            } else {
+                rights.addChild(n);
+            }
+        }
+
+        ConflictOperation<T> conflictOp = new ConflictOperation<>(
+                lefts, rights, target, l.getName(), r.getName(), null, true);
+        conflictOp.apply(context);
     }
 }
